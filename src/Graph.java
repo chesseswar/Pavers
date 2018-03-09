955959357d2d86f518cc0686f5ffec45cee83f44
chesseswar <chesseswar@gmail.com>
2018-03-09 16:27:55 -0600

    init (finished tree, need to make algorithm)

--- src/Graph.java
+++ src/Graph.java
@@ -2,18 +2,47 @@ import java.util.*;
 
 public class Graph {
     int size, edges;
-    LinkedList<Integer>[] vertices;
-    LinkedList<Integer>[] weights;
+    PriorityQueue<Node>[] vertices;
 
     public Graph(int numVert){
-        int size = numVert;
-        vertices = new LinkedList[numVert];
-        weights = new LinkedList[numVert];
+        size = numVert;
+        edges = 0;
+        vertices = new PriorityQueue[numVert];
         for (int i = 0; i < size; i++){
-            vertices[i] = new LinkedList<>();
-            weights[i] = new LinkedList<>();
+            vertices[i] = new PriorityQueue<>();
         }
     }
 
+    public void addEdge(int first, int second, int weight){
+        vertices[first].add(new Node(second, weight));
+        vertices[second].add(new Node(first, weight));
+        edges++;
+    }
 
+    public String toString(){
+        String output = "";
+        for (int i = 0; i < size; i++){
+            output += vertices[i] + "\n";
+        }
+        return output;
+    }
+
+    public class Node implements Comparable<Node>{
+        int value;
+        int weight;
+
+        public Node(int v, int w){
+            value = v;
+            weight = w;
+        }
+
+        public int compareTo(Node other){
+            return Integer.compare(this.weight, other.weight);
+        }
+
+        public String toString(){
+            String output = "<" + value + "," + weight + ">";
+            return output;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,26 @@ public class Main {
             int price = in.nextInt();
             int buildings = in.nextInt();
             int streets = in.nextInt();
+            Graph graph = new Graph(buildings);
+            HashMap<Integer, Integer> streetCodes = new HashMap<>();
+            int index = 0;
+            for (int j = 0; j < streets; j++){
+                int first = in.nextInt();
+                int second = in.nextInt();
+                int weight = in.nextInt();
+                if (!streetCodes.containsKey(first)){
+                    streetCodes.put(first, index);
+                    index++;
+                }
+
+                if (!streetCodes.containsKey(second)){
+                    streetCodes.put(second, index);
+                    index++;
+                }
+
+                graph.addEdge(streetCodes.get(first), streetCodes.get(second), weight);
+            }
+            System.out.println(graph);
         }
     }
 }
